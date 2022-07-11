49b30dd28ddd55101ad4867fb178c6a029b6c310
suhila1010 <sohilaahmed678@gmail.com>
2022-07-12 00:17:48 +0200

    server-side-rendering

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import { render } from "react-dom";
 //import SearchParams from "./SearchPrrams";
 import { StrictMode, useState, lazy, Suspense } from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 //import Details from "./Details";
 import ThemeContext from "./ThemeContext";
 
@@ -24,20 +23,15 @@ const App = () => {
                 "url(http://pets-images.dev-apis.com/pets/wallpaperB.jpg)",
             }}
           >
-            <BrowserRouter>
-              <header className="w-full mb-10 text-center p-7 bg-gradient-to-b from-purple-400 via-pink-500 to-red-500">
-                <Link
-                  className="text-6xl text-white hover:text-gray-200"
-                  to="/"
-                >
-                  Adopt Me!
-                </Link>
-              </header>
-              <Routes>
-                <Route path="/details/:id" element={<Details />} />
-                <Route path="/" element={<SearchParams />} />
-              </Routes>
-            </BrowserRouter>
+            <header className="w-full mb-10 text-center p-7 bg-gradient-to-b from-purple-400 via-pink-500 to-red-500">
+              <Link className="text-6xl text-white hover:text-gray-200" to="/">
+                Adopt Me!
+              </Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
           </div>
         </ThemeContext.Provider>
       </Suspense>
@@ -45,4 +39,6 @@ const App = () => {
   );
 };
 
-render(<App />, document.getElementById("root"));
+//render(<App />, document.getElementById("root"));
+
+export default App;
